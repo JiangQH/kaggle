40d326e05b9f307ee168ededccdb032bac9333b2
JiangQinhong <qinhongjiang@zju.edu.cn>
2016-11-09 16:55:45 +0800

    preprocess

--- DigitRecognizer/src/logistic_regression.py
+++ DigitRecognizer/src/logistic_regression.py
@@ -2,3 +2,6 @@
 maybe the simplest model to do the classification job.
 logistic regression
 """
+from tools import DataHandler
+from sklearn import linear_model
+
--- DigitRecognizer/src/tools.py
+++ DigitRecognizer/src/tools.py
@@ -2,46 +2,54 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_data(train_path, test_path):
+
+class DataHandler:
     """
-    :param train_path: the path to train file
-    :param test_path: the path to test file
-    :return: train_X, train_y, test_X
+    Simple data handler, used to load, write and display the data
     """
-    train = pd.read_csv(train_path)
-    test = pd.read_csv(test_path)
+    def __init__(self):
+        pass
 
-    train_X = train.iloc[:, 1:].values
-    train_X = np.float32(train_X) # each row is a pic
-    train_y = train.iloc[:, 0].values
-    train_y = np.int64(train_y) # each row is a label
+    def load_data(self, train_path, test_path):
+        """
+        :param train_path: the path to train file
+        :param test_path: the path to test file
+        :return: train_X, train_y, test_X
+        """
+        train = pd.read_csv(train_path)
+        test = pd.read_csv(test_path)
 
-    test_X = test.values
-    test_X = np.float32(test_X)
+        train_X = train.iloc[:, 1:].values
+        train_X = np.float32(train_X) # each row is a pic
+        train_y = train.iloc[:, 0].values
+        train_y = np.int64(train_y) # each row is a label
 
-    return train_X, train_y, test_X
+        test_X = test.values
+        test_X = np.float32(test_X)
+
+        return train_X, train_y, test_X
 
 
-def vis_data(data, label):
-    """
-    :param data:  the X feature data (m * n)
-    :param label: the corresponding label (m * 1)
-    :return: none
-    """
-    length = len(label)
-    count = 8 # the number of pics to show each row
-    for i in range(0, count):
-        for j in range(0, count):
-            idx = j * count + i + 1
-            index = np.random.randint(0, length)
-            l = label[index]
-            img = data[index].reshape((28, 28))
-            plt.subplot(count, count, idx)
-            plt.imshow(np.uint8(img))
-            plt.title(l)
-            plt.axis("off")
-            plt.subplots_adjust(wspace = 1.5)
-    plt.show()
+    def vis_data(data, label):
+        """
+        :param data:  the X feature data (m * n)
+        :param label: the corresponding label (m * 1)
+        :return: none
+        """
+        length = len(label)
+        count = 8 # the number of pics to show each row
+        for i in range(0, count):
+            for j in range(0, count):
+                idx = j * count + i + 1
+                index = np.random.randint(0, length)
+                l = label[index]
+                img = data[index].reshape((28, 28))
+                plt.subplot(count, count, idx)
+                plt.imshow(np.uint8(img))
+                plt.title(l)
+                plt.axis("off")
+                plt.subplots_adjust(wspace = 1.5)
+        plt.show()
 
 
 
